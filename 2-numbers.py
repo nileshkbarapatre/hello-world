51e518322945e8b9e7a6a75dfab5a16b81b95bd3
nileshkbarapatre <nileshkbarapatre@gmail.com>
2018-06-02 14:24:25 +0530

    Chapter 1-4

---
+++ 2-numbers.py
@@ -0,0 +1,9 @@
+x=1             #int
+y=2.2           #float
+z="nilesh"      #string
+zx=5j           #complex
+
+print(type(x));
+print(type(y));
+print(type(z));
+print(type(zx));
---
+++ 3-casting.py
@@ -0,0 +1,25 @@
+#integers
+x=int(1)
+y=int(1.1)
+z=int("3")
+print(x)
+print(y)
+print(z)
+
+
+x=float(1)
+y=float(1.1)
+z=float("3")
+w=float("3.2")
+print(x)
+print(y)
+print(z)
+print(w)
+
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+print(x)
+print(y)
+print(z)
